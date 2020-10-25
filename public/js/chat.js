ec9210ec1e204740c249bcca8205b0dcf2dbe50e
ParamjitBaweja <paramjitsinghj0@gmail.com>
2020-10-25 19:08:41 +0530

    Awaiting CSS

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,9 +5,28 @@ const messageForm = document.querySelector('#messageData')
 const formInput = messageForm.querySelector('input')
 const formButton = messageForm.querySelector('button')
 const sendLocationButton = document.querySelector('#send-location')
+const messages = document.querySelector('#messages')
+
+//Templates
+const messageTemplate= document.querySelector('#message-template').innerHTML
+const locationMessageTemplate= document.querySelector('#locationMessage-template').innerHTML
 
 socket.on('message', (message)=>{
     console.log(message)
+    const html = Mustache.render(messageTemplate,{
+        message: message.text,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    })
+    messages.insertAdjacentHTML('beforeend',html)
+})
+
+socket.on('locationMessage', (msg)=>{
+    console.log(msg)
+    const html = Mustache.render(locationMessageTemplate,{
+        url: msg.url,
+        createdAt: moment(msg.createdAt).format('h:mm a')
+    })
+    messages.insertAdjacentHTML('beforeend',html)
 })
 
 messageForm.addEventListener('submit',(e)=>{
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const path = require('path')
 const http = require('http')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
+const {generateMessage,
+generateLocationMessage}=require('./utils/messages')
+
 
 //Hello from the other side
 
@@ -18,10 +21,10 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection',(socket)=>{
     console.log('new websocket connection')
 
-    socket.emit('message','Welcome!')
+    socket.emit('message',generateMessage('Welcome'))
     //sends a welcome message to a user that joins
 
-    socket.broadcast.emit('message','A new user has joined')
+    socket.broadcast.emit('message',generateMessage('A new user has joined'))
     //sends a message to all clients, except the new client,
     //that a new client has joined
 
@@ -32,19 +35,19 @@ io.on('connection',(socket)=>{
             return callback('Profanity is not allowed!')
         }
 
-        io.emit('message', msg)
+        io.emit('message', generateMessage(msg))
         callback()
     })
     //accepts what one cliet sends
     //and sends it across to all the other clients
 
     socket.on('disconnect',()=>{
-        io.emit('message','A user has left')
+        io.emit('message',generateMessage('A user has left'))
     })
     //sends a message to all connected clients that 
     //one client has diconnected
     socket.on('sendLocation',(coords,callback)=>{
-        io.emit('message',`http://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        io.emit('locationMessage',generateLocationMessage(`http://google.com/maps?q=${coords.latitude},${coords.longitude}`))
         callback()
     })
     //shares the location as sent by the client, to all the clients
